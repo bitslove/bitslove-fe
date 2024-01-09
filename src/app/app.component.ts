ea452e506793c6c183cc6cdacbba05f30e5f9366
Flavio Illanes <flavio.illanes@vallegrande.edu.pe>
2024-01-09 16:11:06 -0500

    FI: update configurations, add LigthBox & Bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,16 @@ import { PhotoService } from './photo.service';
 import {  RouterOutlet } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { Lightbox, LightboxModule } from 'ngx-lightbox';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet,
-    HttpClientModule,CommonModule,
+    HttpClientModule,CommonModule,LightboxModule
     ], 
   styleUrls: ['./app.component.css'], 
   templateUrl: './app.component.html',
-  providers: [PhotoService],
+  providers: [PhotoService,Lightbox],
   standalone: true,
 })
 export class AppComponent {
--- src/app/collage/collage.component.ts
+++ src/app/collage/collage.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PhotoService } from '../photo.service';
 import { CommonModule } from '@angular/common'; 
-
+import { Lightbox } from 'ngx-lightbox';
 @Component({
   selector: 'app-collage',
   templateUrl: './collage.component.html',
@@ -14,10 +14,26 @@ import { CommonModule } from '@angular/common';
 
 export class CollageComponent implements OnInit {
   photoUrls: string[] = [];
+   _album:any = [];
+  
 
-  constructor(private PhotoService: PhotoService) {}
+
+  constructor(private PhotoService: PhotoService,private _lightbox: Lightbox) {
+    this._album.push({
+      src: 'https://mdbootstrap.com/img/Photos/Slides/img%20(22).webp',
+      caption: 'Descripción de la imagen 1'
+    }, {
+      src: 'https://mdbootstrap.com/img/Photos/Slides/img%20(22).webp',
+      caption: 'Descripción de la imagen 2'
+    }, {
+      src: 'https://mdbootstrap.com/img/Photos/Slides/img%20(22).webp',
+      caption: 'Descripción de la imagen 3'
+    });
+
+  }
 
   ngOnInit(): void {
+
      this.PhotoService.getCollagePhotos().subscribe(
        (urls: string[]) => {
          this.photoUrls = urls;
@@ -27,6 +43,16 @@ export class CollageComponent implements OnInit {
          console.error('Error fetching photos', error);
        }
      );
+  }
+
+  open(index: number): void {
+    // abre la imagen en el índice proporcionado
+    this._lightbox.open(this._album, index);
+  }
+
+  close(): void {
+    // cierra la galería
+    this._lightbox.close();
   }
 }
 
